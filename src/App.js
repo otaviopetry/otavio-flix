ec45838e9a9f6767d5fff792613b5b9e683300d3
otaviopetry <otaviopetry@gmail.com>
2020-08-01 23:26:57 -0300

    chore: Initial commit
    Application basic structure provided by Alura

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+import initialData from './data/initial_data.json';
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      
+      <BannerMain
+        videoTitle={initialData.categorias[0].videos[0].titulo}
+        url={initialData.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end?"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={initialData.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={initialData.categorias[1]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={initialData.categorias[2]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={initialData.categorias[3]}
+      />
+
+      <Footer />
+
     </div>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './styles.css';
 
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
+
 import logoImg from '../../assets/img/logo.png';
 
 function Menu () {
     return (
-        <header>
-            <img class="logo" src={logoImg} alt="Logotipo otavio.flix" />
-        </header>
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={logoImg} alt="Logotipo otavio.flix" />
+            </a>
+
+            <Button>
+                Novo vídeo
+            </Button>
+        </nav>
     )
 }
 export default Menu;
