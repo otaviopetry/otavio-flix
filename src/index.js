8ea9b942ad3541b20cdbac5d8e08370f187c8dbc
otaviopetry <otaviopetry@gmail.com>
2020-08-04 15:42:32 -0300

    fix(server): Db file location

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AddVideo from './pages/register/Video';
 import AddCategory from './pages/register/Category';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path ="/" exact component={Home} />
-      <Route path ="/cadastro/video" component={AddVideo} />
-      <Route path ="/cadastro/categoria" component={AddCategory} />
-      <Route component={() => { return <h1>404 - Not found.</h1>}} />
+      <Route path="/" exact component={Home} />
+      <Route path="/cadastro/video" component={AddVideo} />
+      <Route path="/cadastro/categoria" component={AddCategory} />
+      <Route component={() => <h1>404 - Not found.</h1>} />
     </Switch>
-  </BrowserRouter>,  
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
